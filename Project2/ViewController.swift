a714f37d1b64f812a58e15bcb473b179c45551d8
Kristin Fan <kfan@seekasia.com>
2023-03-29 15:56:07 +0800

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Kristin Fan on 29/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
